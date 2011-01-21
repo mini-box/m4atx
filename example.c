6ab2faf0749efe07bc2fcb5975c7698cc1bc2474
Ken Tossell <ken@tossell.net>
2011-01-20 22:55:49 -0500

    Add a warning about modifying settings, and let the user cancel.

--- example.c
+++ example.c
@@ -7,9 +7,19 @@ int main (int argc, char **argv) {
   struct m4Diagnostics diag;
 
   struct usb_dev_handle *dev;
-  
+  char user_buf[64];
+
+  printf("This example will set your VIN_MIN_ON to 10 V, and it will\n"
+         "increase IGN_DBC by 10 ms. OK? Type 'yes' or 'no': ");
+
+  if (NULL == fgets(user_buf, sizeof(user_buf), stdin)
+      || user_buf[0] != 'y') {
+    printf("Exiting.\n");
+    return -1;
+  }
+
   dev = m4Init();
-  
+
   if (!dev) {
     perror("m4Init");
     return -1;
@@ -27,7 +37,7 @@ int main (int argc, char **argv) {
     return -1;
 
   printf("VIN_MIN_ON was %0.2f", fval);
- 
+
   fval = 10.0;
   if (m4SetFloat(dev, M4_VIN_MIN_ON, fval))
     return -1;
